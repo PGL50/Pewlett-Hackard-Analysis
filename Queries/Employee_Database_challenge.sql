cbac0a66b33f9d22a8e5450c35b29db8146e247a
PGL50 <84110521+PGL50@users.noreply.github.com>
2021-08-10 06:38:44 -0500

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,6 +1,6 @@
 SELECT e.emp_no,
     e.first_name,
-	e.last_name,
+    e.last_name,
     t.title,
     t.from_date,
     t.to_date
@@ -13,7 +13,7 @@ ORDER by e.emp_no ;
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
     rt.first_name,
-	rt.last_name,
+    rt.last_name,
     rt.title
 INTO unique_titles
 FROM retirement_titles rt
